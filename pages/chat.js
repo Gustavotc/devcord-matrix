6d55df1c0598aa34fa0d432de7a08dd14a780155
Gustavo <gustavotch@hotmail.com>
2022-01-26 21:21:43 -0300

    Tela de chat, botão de enviar e deletar mensagem

--- pages/chat.js
+++ pages/chat.js
@@ -1,3 +1,204 @@
-export default function ChatScreen() {
-	return <div>Página do Chat</div>;
+import { Box, Text, TextField, Image, Button } from '@skynexui/components';
+import React from 'react';
+import appConfig from '../config.json';
+
+export default function ChatPage() {
+	const [message, setMessage] = React.useState('');
+	const [messageList, setMessageList] = React.useState([]);
+
+	function handleNewMessage(newMessage) {
+		if (newMessage.length > 0) {
+			const message = {
+				id: messageList.length + 1,
+				text: newMessage,
+				from: 'Gustavo',
+			};
+			setMessageList([message, ...messageList]);
+			setMessage('');
+		}
+	}
+
+	function handleDeleteMessage(messageId) {
+		//const index = messageList.indexOf((message) => message.id === messageId);
+		setMessageList(messageList.filter((message) => message.id !== messageId));
+	}
+
+	return (
+		<Box
+			styleSheet={{
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+				backgroundColor: appConfig.theme.colors.primary[500],
+				backgroundImage: `url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)`,
+				backgroundRepeat: 'no-repeat',
+				backgroundSize: 'cover',
+				backgroundBlendMode: 'multiply',
+				color: appConfig.theme.colors.neutrals['000'],
+			}}
+		>
+			<Box
+				styleSheet={{
+					display: 'flex',
+					flexDirection: 'column',
+					flex: 1,
+					boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+					borderRadius: '5px',
+					backgroundColor: appConfig.theme.colors.neutrals[700],
+					height: '100%',
+					maxWidth: '95%',
+					maxHeight: '95vh',
+					padding: '32px',
+				}}
+			>
+				<Header />
+				<Box
+					styleSheet={{
+						position: 'relative',
+						display: 'flex',
+						flex: 1,
+						height: '80%',
+						backgroundColor: appConfig.theme.colors.neutrals[600],
+						flexDirection: 'column',
+						borderRadius: '5px',
+						padding: '16px',
+					}}
+				>
+					<MessageList messages={messageList} onDeleteClick={handleDeleteMessage} />
+
+					<Box
+						as='form'
+						styleSheet={{
+							display: 'flex',
+							alignItems: 'center',
+						}}
+						onSubmit={(event) => {
+							event.preventDefault();
+							handleNewMessage(message);
+						}}
+					>
+						<TextField
+							value={message}
+							onChange={(event) => {
+								const value = event.target.value;
+								setMessage(value);
+							}}
+							onKeyPress={(event) => {
+								if (event.key === 'Enter') {
+									event.preventDefault();
+									handleNewMessage(message);
+								}
+							}}
+							placeholder='Insira sua message aqui...'
+							type='textarea'
+							styleSheet={{
+								width: '100%',
+								border: '0',
+								resize: 'none',
+								borderRadius: '5px',
+								padding: '6px 8px',
+								backgroundColor: appConfig.theme.colors.neutrals[800],
+								marginRight: '12px',
+								color: appConfig.theme.colors.neutrals[200],
+							}}
+						/>
+						<Button
+							type='submit'
+							iconName='play'
+							variant='secondary'
+							colorVariant='positive'
+						/>
+					</Box>
+				</Box>
+			</Box>
+		</Box>
+	);
+}
+
+function Header() {
+	return (
+		<>
+			<Box
+				styleSheet={{
+					width: '100%',
+					marginBottom: '16px',
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'space-between',
+				}}
+			>
+				<Text variant='heading5'>Chat</Text>
+				<Button variant='tertiary' colorVariant='neutral' label='Logout' href='/' />
+			</Box>
+		</>
+	);
+}
+
+function MessageList(props) {
+	return (
+		<Box
+			tag='ul'
+			styleSheet={{
+				overflow: 'scroll',
+				display: 'flex',
+				flexDirection: 'column-reverse',
+				flex: 1,
+				color: appConfig.theme.colors.neutrals['000'],
+				marginBottom: '16px',
+			}}
+		>
+			{props.messages.map((message, onDeleteClick) => {
+				return (
+					<Text
+						key={message.id}
+						tag='li'
+						styleSheet={{
+							borderRadius: '5px',
+							padding: '6px',
+							marginBottom: '12px',
+							hover: {
+								backgroundColor: appConfig.theme.colors.neutrals[700],
+							},
+						}}
+					>
+						<Box
+							styleSheet={{
+								marginBottom: '8px',
+							}}
+						>
+							<Image
+								styleSheet={{
+									width: '20px',
+									height: '20px',
+									borderRadius: '50%',
+									display: 'inline-block',
+									marginRight: '8px',
+								}}
+								src={`https://github.com/gustavotc.png`}
+							/>
+							<Text tag='strong'>{message.from}</Text>
+							<Text
+								styleSheet={{
+									fontSize: '10px',
+									marginLeft: '8px',
+									color: appConfig.theme.colors.neutrals[300],
+								}}
+								tag='span'
+							>
+								{new Date().toLocaleDateString()}
+							</Text>
+							<Button
+								type='button'
+								iconName='trash'
+								variant='tertiary'
+								colorVariant='negative'
+								onClick={() => props.onDeleteClick(message.id)}
+							/>
+						</Box>
+						{message.text}
+					</Text>
+				);
+			})}
+		</Box>
+	);
 }
